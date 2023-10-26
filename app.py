f3bc0c35b3b33d27544f461641c03609d5f00ca7
ouaga_dedi <ouagadedi95@gmail.com>
2023-10-26 16:43:49 +0000

    retouch deploy

--- app.py
+++ app.py
@@ -265,25 +265,6 @@ def soumettre():
 
 
 
-@app.route('/', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('upload.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
-
 
 
 
